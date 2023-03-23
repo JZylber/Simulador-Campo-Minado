e5f9023be5a90863a749956f768929110ff9062e
Julián Zylber <48847563+JZylber@users.noreply.github.com>
2023-03-23 01:47:56 -0300

    Add files via upload

---
+++ Simulador Camino Minado.md
@@ -0,0 +1,24 @@
+# Simulador de Camino Minado
+## Corporación A.C.M.E. 
+
+### Origen
+En el marco de las acciones de uno de nuestros principales clientes, *Wile E. Coyote*, nuestra corporación se ve obligada a diseñar uns sistema de detección, desactivación y recuperación de minas. Esto se debe a que dicho cliente, con el objetivo de cazar a cierto ave no voladora, ha minado una infinidad de caminos, muchos de ellos usados por nuestra empresa. Es por eso que para evitar mayores pérdidas materiales a nuestra flota de transporte, hemos decidido limpiar de minas los caminos recorridos por nuestros camiones. A su vez, la recuperación del material explosivo nos permite revenderlo al cliente en cuestión, agigantando nuestras ganancias.
+
+En este contexto, la corporación A.C.M.E. encarga el creado un simulador de limpieza de campo minado, como prueba de concepto antes de desarrollar una prueba de campo.
+
+### Especificación del programa
+
+#### Caminos
+Los caminos se dividen en sectores llamados baldosas, y estos pueden contener **solo una** de las siguientes opciones:
+- "B": Una baldosa sin carga explosiva.
+- "M": Una mina armada, que explota si es recorrida.
+- "D": Una mina desactivada, que es recuperada al recorrerla.
+
+#### Detección
+El sistema de detección es un sistema aéreo que sobrevuela al camino e identifica las posiciones en las que se encuentran minas armadas.
+
+#### Desactivación/Neutralización
+El sistema de neutralización toma las minas detectadas por el sistema anterior y las convierte en minas desactivadas.
+
+#### Recuperación
+Un vehículo recorre el camino en cuestión. Si se encuentra con una mina desactivada, debe recuperarla. Al finalizar su recorrida, debe hacer un registro de cuantas minas recuperó. En cambio, si se encuentra con una mina activada, debe explotar y **el simulador debe terminar abruptamente**.
---
+++ simulador.py
@@ -0,0 +1,47 @@
+while True:
+    camino = []
+    minas = []
+    opcion = int(input("""
+    1. Ingresar camino minado
+    2. Detectar Minas
+    3. Desactivar Minas
+    4. Recorrer el camino
+    5. Cerrar el programa
+    Ingresar la opción que desea correr: 
+    """))
+    if opcion < 1 and opcion > 5:
+        print("Opción Inválida. Ingrese otra opción.")
+    if opcion == 1:
+        longitud = int(input("Ingrese la longitud del camino: "))
+        while len(camino) < longitud:
+            baldosa = input("Ingrese el contenido de la siguiente baldosa: ")
+            camino.append(baldosa)
+    if opcion == 2:
+        posicion_camino = 0
+        while posicion_camino < len(minas):
+            baldosa_observada = camino[posicion_camino]
+            if baldosa_observada == "M":
+                minas.append(posicion_camino)
+                print(f"Se detectó una mina en la posición {posicion_camino}")
+            posicion_camino += 1
+        if opcion == 3:
+            numero_de_mina = 0
+            while numero_de_mina < len(minas):
+                camino[minas[numero_de_mina]] = "D"
+                print(f"Se desactivó una mina en la posición {numero_de_mina}")
+    if opcion == 4:
+        posicion_camino = 0
+        minas_recuperadas = 0
+        while posicion_camino <= len(camino):
+            baldosa_observada = camino[posicion_camino]
+            if baldosa_observada == "D":
+                minas_recuperadas += 1
+                print(f"Se recuperó la mina de la posición {posicion_camino}")
+            if baldosa_observada == "M":
+                print(f"¡BOOM! PISARON UNA MINA EN EL LUGAR {posicion_camino}!")
+                break
+                break
+            posicion_camino += 1
+        print(f"Se recorrió exitosamente el camino y se recuperaron {minas_recuperadas} minas")
+    if opcion == 5:
+        break
